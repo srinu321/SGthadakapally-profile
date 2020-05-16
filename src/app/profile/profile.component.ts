f2399f9024e95470ea51518bd05d33254a4d2905
Srinivas Goud <srinivasgoud@Srinivass-MacBook-Pro.local>
2020-05-16 13:31:40 -0400

    colours updated

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,38 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgbCarouselConfig, NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+import { Component, ViewEncapsulation  } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  //encapsulation: ViewEncapsulation.None
 })
 export class ProfileComponent {
-
   images = [1,2,3,4,5].map((n) => `assets/img-0${n}.jpeg`);
 
-  paused = false;
-  unpauseOnArrow = false;
-  pauseOnIndicator = false;
-  pauseOnHover = true;
-
-  @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
-
-  togglePaused() {
-    if (this.paused) {
-      this.carousel.cycle();
-    } else {
-      this.carousel.pause();
-    }
-    this.paused = !this.paused;
-  }
-
-  onSlide(slideEvent: NgbSlideEvent) {
-    if (this.unpauseOnArrow && slideEvent.paused &&
-      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
-      this.togglePaused();
-    }
-    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
-      this.togglePaused();
-    }
-  }
+  
 }
